2bf0c7ffd55293d5cad29105defa03dfe2ed7912
nidhin sanju <nidhinsanjuk@gmail.com>
2024-02-28 20:15:52 +0530

    Working as expected

--- Client/src/utis/CheckoutButton.jsx
+++ Client/src/utis/CheckoutButton.jsx
@@ -1,20 +1,35 @@
+import { useNavigate } from "react-router-dom";
 import { SERVER_URL } from "../Constents/URL";
+import axios from "axios";
 
-export async function CheckoutButton() {
-  const customerID = localStorage.getItem("customerID");
+export function CheckoutButton() {
+  const navigate = useNavigate();
+  const customerID = localStorage.getItem("CustomerID");
+  const Checkout = async () => {
+    try {
+      const res = await axios.put(SERVER_URL + "/checkout", {
+        CustomerID: customerID,
+      });
+      if (res.status === 200) {
+        alert("Checked out Successfully");
+        navigate("/");
+      }
+      localStorage.removeItem("token");
+      localStorage.removeItem("CustomerID");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      alert("Something went wrong");
+    }
+  };
   return (
-    <div>
+    <div className="mt-5">
       <button
         onClick={() => {
-          const res = axios.put(SERVER_URL + "/checkout", {
-            CustomerID: customerID,
-          });
-          if (res.status === 200) {
-            alert("Checked out Successfully");
-          }
+          Checkout();
         }}
         type="button"
-        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
       >
         Check-Out
       </button>
